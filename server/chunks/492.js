704229878db0e54aba23e548f0ccd743c38c2203
mohazerro <mohazerro@github.com>
2023-12-01 01:03:19 +0100

    testdeploy22

---
+++ server/chunks/492.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.id = 492;
+exports.ids = [492];
+exports.modules = {
+
+/***/ 492:
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"G":[{"title":"About","url":"http://www.example.com/about"},{"title":"Projects","url":"http://www.example.com/portfolio"},{"title":"Contact","url":"http://www.example.com/contact"}],"rP":{"name":"Gamil Mohamed","subtitle":"I\'m a young developer."},"jZ":{"title":"About me","image":"/mgamil.jpg","primary":"24 years old, passionate about technology, I\'m a student at 42 and I like to learn new things and share my knowledge with others","secondary":"Always looking for new challenges, I am a person who likes to work in a team and I am always ready to help others"},"q":{"title":"Projects","projects":[{"title":"Minishell","description":"The objective of this project is for you to create a simple shell. Learnt a lot about file descriptors, process and bash. Coded in C.","url":"http://www.github.com/mohazerro/Minishell","github":"http://www.github.com/mohazerro/Minishell","image":"/clebon.gif"},{"title":"Cub3D","description":"Interesting project where I learnt how raycasting works and how to render a 3D environment with a 2D map. Coded in C with the minilibx library.","url":"http://www.github.com/mohazerro/Cub3D","github":"http://www.github.com/mohazerro/Cub3D","image":"/cub3d.mp4"},{"title":"Project 3","description":"If the project was collaborative, reflect it in this description, that will demonstrate communication and/or leadership skills. Additionally, if you made use of the mastery of a second language, it will reflect on you professionalism.","url":"http://www.github.com/mohazerro/","github":"http://www.github.com/mohazerro/","image":"/placeholder-700x350.png"}]},"PX":{"title":"Contact","description":"This is where you can describe about your contact. The more you describe about your contact, the more chances you have!","formId":"your-form-id"},"Mv":{"linkedin":"","twitter":"","github":""}}');
+
+/***/ })
+
+};
+;
